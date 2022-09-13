7e0fa66a360e2ce4d983937d096cedb809463223
Aldana Martin <aldimartin1990@gmail.com>
2022-09-12 21:13:09 -0300

    Se entrega desafio DOM

---
+++ js/dom.js
@@ -0,0 +1,67 @@
+let carritoDeCompras = [];
+
+// Llamadas al HTML:
+let contenedor = document.getElementById("main_container");
+let alCarrito = document.getElementById("carrito");
+
+// Creo elementos desde JS a DOM-HTML.
+// llamo al array stockProduuctos desde stock.js, y lo itero.
+
+stockProductos.forEach(item =>{
+
+// creo un div con cada uno de los objetos delarray
+let div = document.createElement('div')
+div.className = 'div_container'
+
+// Creo código de JS dentro del div
+div.innerHTML = `<card class="card_container">
+<div class="info_card">
+    <h2 class="title__card">${item.nombre}</h2>
+    <p class="parraph__card">${item.descripcion}</p>
+    <p class="parraph__card">$${item.precio}</p>
+    <div class="input">
+        <input type="number" id="cantidad" placeholder="Ingrese cantidad">
+    </div>
+    <button id="btnAñadir${item.id}" class="button_card" type="button">Añadir al carrito</botton>
+</div>
+</card>`
+
+// Añado el código al cotenedor 
+contenedor.appendChild(div);
+
+// Creo Evento Click para botón de producto
+let btnAñadir = document.getElementById(`btnAñadir${item.id}`);
+btnAñadir.addEventListener('click', ()=> {
+
+// Invoco a la función agregarCarrito
+agregarAlCarrito(item.id);
+})
+});
+
+// Creo función para añadir el producto filtrado al array carritoDeCompras
+function agregarAlCarrito (id) {
+
+// Ingreso a la función y busco coincidencia con id
+let productoAñadido = stockProductos.find(item=> item.id == id);
+
+
+// Realizo Push del producto encontrado en array de objetos y lo pusheo al carritDeCompras
+carritoDeCompras.push(productoAñadido);
+
+// llamo a la función mostrarCarrito
+    mostrarCarrito(productoAñadido)
+}
+
+// función mostrarCarrito
+function mostrarCarrito (productoAñadido) {
+
+// creo un div por cada producto seleccionado en JS
+let div = document.createElement('div');
+div.className = 'carrito';
+div.innerHTML = `<h3 id="text__carrito">${productoAñadido.nombre}</h3>
+                <h4 id="total">Precio: $${productoAñadido.precio}</h4>
+                <button class="btn_eliminar">Eliminar carrito</button>`
+
+// envío el div al HTML
+    alCarrito.appendChild(div)
+}
---
+++ js/stock.js
@@ -0,0 +1,11 @@
+// declaración de stock productos a través de una array deobjetos literales:
+
+let stockProductos = [
+    {id: 1, nombre: "Promo 1", tipo: "promocion", descripcion: " 6 empanadas clásicas + bebida de 500cc", precio: 650},
+    {id: 2, nombre: "Promo 2", tipo: "promocion", descripcion: " 1/2 pizza de Muzza + bebida de 500cc", precio: 650},
+    {id: 3, nombre: "Promo 3", tipo: "promocion", descripcion: " 1/2 pizza de Muzza + 4 empanadas clásicas + 2 bebidas de 500cc", precio: 1100},
+    {id: 4, nombre: "Promo 4", tipo: "promocion", descripcion: " 1 pizza de Muzza + 4 empanadas clásicas + 1 bebida 1,5 Lt", precio: 1650},
+    {id: 5, nombre: "Promo 5", tipo: "promocion", descripcion: " 1 pizza especial + 6 empanadas mixtas + 1 bebida 1,5 Lt", precio: 2050},
+    {id: 6, nombre: "Promo 6", tipo: "promocion", descripcion: " 1 pizza especial + 12 empanadas mixtas + 1 bebida 1,5 Lt", precio: 2590},
+]
+
